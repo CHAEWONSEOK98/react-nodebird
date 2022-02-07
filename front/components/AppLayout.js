8492a3003a518e13411b72c85bb2db3e3a34aa89
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-07 14:39:17 +0900

    dummy data login

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { useState } from "react";
 import { Input, Menu, Row, Col } from "antd";
 import styled from "styled-components";
 import UserProfile from "../components/UserProfile";
@@ -12,7 +11,7 @@ const SearchInput = styled(Input.Search)`
 `;
 
 const AppLayout = ({ children }) => {
-  const { isLoggedIn, setIsLoggedIn } = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <Menu mode="horizontal">
@@ -29,7 +28,7 @@ const AppLayout = ({ children }) => {
         </Menu.Item>
 
         <Menu.Item>
-          <SearchInput enterButton style={{ verticalAlign: "middle" }} />
+          <SearchInput enterButton />
         </Menu.Item>
 
         <Menu.Item>
@@ -40,7 +39,11 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+          {isLoggedIn ? (
+            <UserProfile setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
         </Col>
         <Col xs={24} md={12}>
           {children}
--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -3,26 +3,35 @@ import { Form, Input, Button } from "antd";
 import Link from "next/link";
 import styled from "styled-components";
 
-const ButtonWrapper = styled.did`
+const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
 
-const LoginForm = () => {
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`;
+
+const LoginForm = ({ setIsLoggedIn }) => {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
 
   const onChangeId = useCallback((e) => {
+    console.log(e.target.value);
     setId(e.target.value);
   }, []);
 
   const onChangePassword = useCallback((e) => {
+    console.log(e.target.value);
     setPassword(e.target.value);
   }, []);
 
-  const style = useMemo(() => ({ marginTop: 10 }), []);
+  const onSubmitForm = useCallback(() => {
+    console.log(id, password);
+    setIsLoggedIn(true);
+  }, [id, password]);
 
   return (
-    <Form>
+    <FormWrapper onFinish={onSubmitForm}>
       <div>
         <label htmlFor="user-id">아이디</label>
         <br />
@@ -39,7 +48,7 @@ const LoginForm = () => {
           required
         />
       </div>
-      <ButtonWrapper style={style}>
+      <ButtonWrapper>
         <Button type="primary" htmlType="submit" loading={false}>
           로그인
         </Button>
@@ -49,7 +58,7 @@ const LoginForm = () => {
           </a>
         </Link>
       </ButtonWrapper>
-    </Form>
+    </FormWrapper>
   );
 };
 
