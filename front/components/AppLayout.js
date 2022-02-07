a95234436b6765c2ad23ff5b70e669a71938621c
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-07 13:32:59 +0900

    add LoginForm

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,9 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { useState } from "react";
+import { Input, Menu, Row, Col } from "antd";
+import styled from "styled-components";
+import UserProfile from "../components/UserProfile";
+import LoginForm from "../components/LoginForm";
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
+  const { isLoggedIn, setIsLoggedIn } = useState(false);
   return (
     <div>
       <Menu mode="horizontal">
@@ -20,12 +29,32 @@ const AppLayout = ({ children }) => {
         </Menu.Item>
 
         <Menu.Item>
+          <SearchInput enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+
+        <Menu.Item>
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://github.com/CHAEWONSEOK98"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Made by ChaeGyul
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
